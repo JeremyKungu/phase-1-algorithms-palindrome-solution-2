38507ec59e2118cb755ccad85dbf0f224c4df0ba
JeremyKungu <jeremikungu27@gmail.com>
2022-08-25 19:57:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let length = word.length;
+  
+  for (let i = 0; i < length / 2; i++) {
+    const j = length - 1 - i;
+
+    if(word[i] !== word[j]) {
+      return false;
+    }
+  }
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+isPalindrome("racecar");
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
